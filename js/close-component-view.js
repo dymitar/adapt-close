0f9a461ad398be3cdcf229a6c5d2e3b58182c783
RobertPeek <robert@delta-net.co.uk>
2016-12-21 11:00:28 +0000

    Fixes #5  Fixes #6

--- js/close-component-view.js
+++ js/close-component-view.js
@@ -9,6 +9,7 @@ define(function(require) {
 
         initialize: function () {
             this.render();
+            this.listenTo(Adapt, "close:closeWindow", this.onCloseConfirm);
         },
 
         events: {
@@ -29,8 +30,35 @@ define(function(require) {
 
         closeModule: function(event) {
             if (event) event.preventDefault();
-            Adapt.trigger("close:confirm");
-        }
+            // Check for prompt enabled
+            if(this.model.get('_close')._notifyPrompt._isEnabled) {
+              this.showPrompt();
+            } else {
+              Adapt.trigger("close:confirm");
+            }
+        },
+
+        showPrompt: function() {
+    			var prompt = this.model.get('_close')._notifyPrompt;
+
+    			Adapt.trigger("notify:prompt", {
+    				title: prompt.title,
+    				body: prompt.body,
+    				_prompts: [
+    					{
+    						promptText: prompt.confirm,
+    						_callbackEvent: "close:closeWindow"
+    					},
+    					{
+    						promptText: prompt.cancel
+    					}
+    				]
+    			});
+    		},
+
+        onCloseConfirm: function() {
+    			top.window.close();
+    		}
 
     });
 
