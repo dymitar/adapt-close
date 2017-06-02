775e7d3c42cf96a8f6b0c1b3d6914bc9fa6772d4
RobertPeek <robert@delta-net.co.uk>
2017-06-02 10:06:58 +0100

    Fixes #9

--- js/close-component-view.js
+++ js/close-component-view.js
@@ -28,7 +28,7 @@ define(function(require) {
             }
 
             // Add data
-            $(this.el).html(template(data)).appendTo('.extensions');
+            $(this.el).html(template(data)).appendTo('.' + this.model.get('_id') + " > .component-inner" + " > .extensions");
 
             _.defer(_.bind(function() {
                 this.postRender();
